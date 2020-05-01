cebe3d1f248b781c6f307a55f6f2e0579d5149e7
Luke Prochazka <tap1r@users.noreply.github.com>
2020-05-01 16:27:13 +1000

    Update oplogchurn.js

--- src/oplogchurn.js
+++ src/oplogchurn.js
@@ -33,6 +33,7 @@ const agg = [ { $match: { ts: { $gte: Timestamp(t2, 1), $lte: Timestamp(t1, 1) }
  * main
  */
 
+rs.slaveOk();
 db = db.getSiblingDB('local');
 const oplog = db.oplog.rs.aggregate(agg)
 oplog.forEach(function (op) {
