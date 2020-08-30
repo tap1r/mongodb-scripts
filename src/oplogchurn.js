053c71ed204684135a035fbaf71b6f0d4ace84e3
Luke Prochazka <tap1r@users.noreply.github.com>
2020-08-30 14:09:33 +1000

    Update oplogchurn.js

--- src/oplogchurn.js
+++ src/oplogchurn.js
@@ -1,7 +1,9 @@
-// Oplog churn rate script
-// - created by luke.prochazka@mongodb.com
+/*
+ * oplog churn rate script
+ * created by luke.prochazka@mongodb.com
+ */
 
-// Usage: "mongo [+options] --quiet oplogchurn.js"
+// Usage: "mongo [+connection options] --quiet oplogchurn.js"
 
 /*
  *  Load helper lib (https://github.com/tap1r/mongodb-scripts/blob/master/src/mdblib.js)
@@ -23,11 +25,11 @@ const rowHeader = 35;
 var total = 0;
 var docs = 0;
 //
-const hrs = 1 // set interval
+const hrs = 1; // set interval
 const d = new Date();
 const t1 = d.getTime() / 1000;
 const t2 = d.setHours(d.getHours() - hrs) / 1000;
-const agg = [ { $match: { ts: { $gte: Timestamp(t2, 1), $lte: Timestamp(t1, 1) } } }, { $project: { _id: 0 } } ]
+const agg = [ { $match: { ts: { $gte: Timestamp(t2, 1), $lte: Timestamp(t1, 1) } } }, { $project: { _id: 0 } } ];
 
 /*
  * main
@@ -35,7 +37,7 @@ const agg = [ { $match: { ts: { $gte: Timestamp(t2, 1), $lte: Timestamp(t1, 1) }
 
 rs.slaveOk();
 db = db.getSiblingDB('local');
-const oplog = db.oplog.rs.aggregate(agg)
+const oplog = db.oplog.rs.aggregate(agg);
 oplog.forEach(function (op) {
     var msg = Object.bsonsize(op);
     total += msg;
@@ -50,11 +52,12 @@ print('='.repeat(termWidth));
 print("Start time:".padEnd(rowHeader), (t1).toString().padStart(columnWidth));
 print("End time:".padEnd(rowHeader), (t2).toString().padStart(columnWidth));
 print("Interval:".padEnd(rowHeader), (hrs + "hr(s)").padStart(columnWidth));
-print("Avg oplog compression ratio:".padEnd(rowHeader), (ratio + ":1").padStart(columnWidth))
-print("Doc count:".padEnd(rowHeader), docs.toString().padStart(columnWidth))
-print("Total Ops size:".padEnd(rowHeader), ((total/scale.factor).toFixed(2) + ' ' + scale.unit).padStart(columnWidth))
-print("Estimated total Ops size on disk:".padEnd(rowHeader), ((total/(scale.factor*ratio)).toFixed(2) + ' ' + scale.unit).padStart(columnWidth))
+print("Avg oplog compression ratio:".padEnd(rowHeader), (ratio + ":1").padStart(columnWidth));
+print("Doc count:".padEnd(rowHeader), docs.toString().padStart(columnWidth));
+print("Total Ops size:".padEnd(rowHeader), ((total/scale.factor).toFixed(2) + ' ' + scale.unit).padStart(columnWidth));
+print("Estimated total Ops size on disk:".padEnd(rowHeader), ((total/(scale.factor*ratio)).toFixed(2) + ' ' + scale.unit).padStart(columnWidth));
 print('-'.repeat(termWidth));
-print("Estimated current oplog churn:".padEnd(rowHeader), ((total/(scale.factor*ratio*hrs)).toFixed(2) + ' ' + scale.unit + "/hr").padStart(columnWidth))
+print("Estimated current oplog churn:".padEnd(rowHeader), ((total/(scale.factor*ratio*hrs)).toFixed(2) + ' ' + scale.unit + "/hr").padStart(columnWidth));
 print('='.repeat(termWidth));
+
 // EOF
