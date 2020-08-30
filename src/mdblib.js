ad658b76321d2b2545cd901333eae264381305be
Luke Prochazka <tap1r@users.noreply.github.com>
2020-08-30 14:14:07 +1000

    Update mdblib.js

--- src/mdblib.js
+++ src/mdblib.js
@@ -1,7 +1,7 @@
 /*
  *  mdblib.js
  *  Mongo shell helper functions
- *  - created by luke.prochazka@mongodb.com
+ *  created by luke.prochazka@mongodb.com
  */
 
 /*
@@ -12,14 +12,14 @@
  */
 
 String.prototype.padStart = function padStart(targetLength, padString) {
-    targetLength = targetLength >> 0; //truncate if number, or convert non-number to 0;
+    targetLength = targetLength >> 0; // truncate if number, or convert non-number to 0;
     padString = String(typeof padString !== 'undefined' ? padString : ' ');
     if (this.length >= targetLength) {
         return String(this);
     } else {
         targetLength = targetLength - this.length;
         if (targetLength > padString.length) {
-            padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+            padString += padString.repeat(targetLength / padString.length); // append to original to ensure we are longer than needed
         }
         return padString.slice(0, targetLength) + String(this);
     }
@@ -33,7 +33,7 @@ String.prototype.padEnd = function padEnd(targetLength, padString) {
     } else {
         targetLength = targetLength - this.length;
         if (targetLength > padString.length) {
-            padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+            padString += padString.repeat(targetLength / padString.length); // append to original to ensure we are longer than needed
         }
         return String(this) + padString.slice(0, targetLength);
     }
