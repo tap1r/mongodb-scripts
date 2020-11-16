07bb1c299ce5e69a7e903ff904496385e5118a12
tap1r <luke.prochazka@gmail.com>
2020-11-16 12:13:31 +1100

    Update oplogchurn.js

--- src/oplogchurn.js
+++ src/oplogchurn.js
@@ -13,14 +13,17 @@
 
 load('mdblib.js');
 
+// User defined parameters
+let hrs = 1; // set interval
+
+// Formatting preferences
+const scale = new ScaleFactor(); // 'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'
+let termWidth = 60, columnWidth = 25, rowHeader = 34;
+
 /*
  * Global defaults
  */
 
-// user defined parameters
-let hrs = 1; // set interval
-
-// init vars
 var total = 0, docs = 0;
 let d = new Date();
 let t2 = Math.floor(d.getTime() / 1000.0); // end timestamp
@@ -37,10 +40,6 @@ let agg = [{
   },{
       $project: { "_id": 0 }
 }];
-
-// Formatting preferences
-const scale = new ScaleFactor(); // 'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'
-let termWidth = 80, columnWidth = 35, rowHeader = 44;
 
 /*
  * main
@@ -81,14 +80,14 @@ let ratio = (stats.size / (stats.storageSize - blocksFree)).toFixed(2);
 print('='.repeat(termWidth));
 print('Start time:'.padEnd(rowHeader), d1.padStart(columnWidth));
 print('End time:'.padEnd(rowHeader), d2.padStart(columnWidth));
-print('Interval:'.padEnd(rowHeader), (hrs + ' hr(s)').padStart(columnWidth));
-print('Total oplog average compression ratio:'.padEnd(rowHeader),
+print('Interval duration:'.padEnd(rowHeader), (hrs + ' hr(s)').padStart(columnWidth));
+print('Average oplog compression ratio:'.padEnd(rowHeader),
       (ratio + ':1').padStart(columnWidth));
 print('Interval document count:'.padEnd(rowHeader),
       docs.toString().padStart(columnWidth));
-print('Interval Ops combined object size:'.padEnd(rowHeader),
+print('Interval data size:'.padEnd(rowHeader),
       ((total / scale.factor).toFixed(2) + ' ' + scale.unit).padStart(columnWidth));
-print('Estimated interval Ops combined disk size:'.padEnd(rowHeader),
+print('Estimated interval storage size:'.padEnd(rowHeader),
       ((total / (scale.factor * ratio)).toFixed(2) + ' ' + scale.unit).padStart(columnWidth));
 print('-'.repeat(termWidth));
 print('Estimated current oplog churn:'.padEnd(rowHeader),
