f382ec3be5342f5a0d95f2d0f6fd232c3b76f53b
tap1r <luke.prochazka@gmail.com>
2021-08-03 18:11:49 +1000

    Update schema-sampler.js
    filter out system collections/views

--- src/schema-sampler.js
+++ src/schema-sampler.js
@@ -1,6 +1,6 @@
 /*
  *  Name: "schema-sampler.js"
- *  Version: "0.1.0"
+ *  Version: "0.1.1"
  *  Description: generate schema data by simulating mongosqld sampling commands
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  */
@@ -12,13 +12,13 @@ var dbs = db.getMongo().getDBNames().filter(dbName => dbName.match(/^(?!(admin|c
 // var dbs = ['namespace']; // restrict list to known namespaces
 var schema = dbs.map(dbName => ({
     "db": dbName,
-    "collections": db.getSiblingDB(dbName).getCollectionInfos({ "type": "collection", "name": { "$ne": "system.views" } }, true, true).map(collection => ({
+    "collections": db.getSiblingDB(dbName).getCollectionInfos({ "type": "collection", "name": { "$not": { "$regex": /^system\./ } } }, true, true).map(collection => ({
         "name": collection.name,
         "documents": db.getSiblingDB(dbName).getCollection(collection.name).stats().count,
         "indexes": db.getSiblingDB(dbName).getCollection(collection.name).getIndexes(),
         "$sample": db.getSiblingDB(dbName).getCollection(collection.name).aggregate(agg).toArray()
     }))[0],
-    "views": db.getSiblingDB(dbName).getCollectionInfos({ "type": "view" }, true, true).map(view => ({
+    "views": db.getSiblingDB(dbName).getCollectionInfos({ "type": "view", "name": { "$not": { "$regex": /^system\./ } } }, true, true).map(view => ({
         "name": view.name,
         "options": view.options,
         "$sample": db.getSiblingDB(dbName).getCollection(view.name).aggregate(agg).toArray()
