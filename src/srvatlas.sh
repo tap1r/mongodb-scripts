c301f2c088872da64acc818c807f8d035be392b6
Luke Prochazka <tap1r@users.noreply.github.com>
2021-08-26 09:33:14 +1000

    v0.1.1 bump
    prettier output formatting

--- src/srvatlas.sh
+++ src/srvatlas.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #
 # Name: "srvatlas.sh"
-# Version = "0.1.0"
-# Description: Atlas SRV record validator
+# Version: "0.1.1"
+# Description: Atlas cluster name validator
 # Authors: ["tap1r <luke.prochazka@gmail.com>"]
 
-_domain="${1:?Usage: srv.sh atlas-srv-name}"
+_domain="${1:?Usage: srvatlas.sh atlas-cluster-name}"
 _txt=$(dig +short $_domain TXT)
 
-echo "Validating Atlas SRV records for ${_domain}"
-echo -e "TXT resource record:\t$_txt"
+echo -e "Validating Atlas cluster name:\t${_domain}"
+echo -e "TXT resource record:\t\t$_txt"
 
 for _srv in $(dig +short _mongodb._tcp.${_domain} SRV); do
   if [ "${_srv: -13}" = ".mongodb.net." ]; then
-    echo "SRV resource record: ${_srv}"
-    echo "Resolves to CNAME/A record:"
-    echo "$(dig +short ${_srv} A)"
+    echo -e "SRV resource record:\t\t${_srv}"
+    echo -e "Resolves to CNAME/A record:\t$(dig +short ${_srv} A)"
   fi
 done
