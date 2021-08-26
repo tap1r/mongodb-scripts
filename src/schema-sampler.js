7ffe7a7c9346d87a1d6338234935b852521ed180
tap1r <luke.prochazka@gmail.com>
2021-08-26 16:22:45 +1000

    version bump
    refactored

--- src/schema-sampler.js
+++ src/schema-sampler.js
@@ -1,6 +1,6 @@
 /*
  *  Name: "schema-sampler.js"
- *  Version: "0.1.2"
+ *  Version: "0.2.0"
  *  Description: generate schema with simulated mongosqld sampling commands
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  */
@@ -9,37 +9,108 @@
 
 __script = {
     "name": "schema-sampler.js",
-    "version": "0.1.2"
+    "version": "0.2.0"
 };
-print('\n', 'Running script', __script.name, ' v' + __script.version, '\n');
+print('\n', 'Running script', __script.name, 'v' + __script.version);
 
-var userOptions = {
+let userOptions = {
     /*
-     *  User preferences
+     *  user preferences
      */
-    readPreference: 'primaryPreferred',
-    sampleSize: 1    // defaults to 1 for performance reasons, increase for sparse data
+    sampleSize: 1, // defaults to 1 for performance reasons, increase for sparse data
+    // dbs: ['namespace'], // restrict list to known namespaces
+    readPreference: 'secondaryPreferred'
 }
 
-db.getMongo().setReadPref(userOptions.readPreference);
-var agg = [{ "$sample": { "size": userOptions.sampleSize } }];
-var dbs = db.adminCommand({ "listDatabases": 1, "nameOnly": true, "filter": { "name": /^(?!(admin|config|local)$)/i }, "authorizedDatabases": true }).databases.map(dbName => dbName.name);
-// var dbs = ['namespace']; // restrict list to known namespaces
-var schema = dbs.map(dbName => ({
-    "db": dbName,
-    "collections": db.getSiblingDB(dbName).getCollectionInfos({ "type": "collection", "name": { "$not": { "$regex": /^system\./ } } }, true, true).map(collection => ({
-        "name": collection.name,
-        "documents": db.getSiblingDB(dbName).getCollection(collection.name).stats().count,
-        "indexes": db.getSiblingDB(dbName).getCollection(collection.name).getIndexes(),
-        "$sample": db.getSiblingDB(dbName).getCollection(collection.name).aggregate(agg).toArray()
-    }))[0],
-    "views": db.getSiblingDB(dbName).getCollectionInfos({ "type": "view", "name": { "$not": { "$regex": /^system\./ } } }, true, true).map(view => ({
-        "name": view.name,
-        "options": view.options,
-        "$sample": db.getSiblingDB(dbName).getCollection(view.name).aggregate(agg).toArray()
-    }))[0]
-}));
+function main(userOptions) {
+    /*
+     *  main
+     */
+    db.getMongo().setReadPref(userOptions.readPreference);
+    // (userOptions.dbs === 'undefined');
+    let schema = getSchema(userOptions.sampleSize);
+    genReport(schema);
 
-print(JSON.stringify(schema, null, '  '));
+    return;
+}
+
+function getSchema(sampleSize) {
+    /*
+     *  generate a synthetic schema with metadata
+     */
+    let pipeline = [{ "$sample": { "size": sampleSize } }];
+    // let comment = 'Executed by ' + __script.name + ' v' + __script.version;
+    let listDbOpts = [
+        {
+            "listDatabases": 1,
+            "filter": { "name": /^(?!(admin|config|local)$)/ },
+            "nameOnly": true,
+            "authorizedDatabases": true
+        },
+        // comment
+    ];
+    // db.runCommand({ "listCollections": 1, "authorizedCollections": true, "nameOnly": true });
+    let listColOpts = [
+        {
+            "type": "collection",
+            "name": { "$not": { "$regex": /^system\./ } }
+        },
+        true,
+        true
+    ];
+    let listViewOpts = [
+        {
+            "type": "view",
+            "name": { "$not": { "$regex": /^system\./ } }
+        },
+        true,
+        true
+    ];
+    let dbs = db.adminCommand(...listDbOpts).databases.map(dbName => dbName.name);
+    return dbs.map(dbName => ({
+            "db": dbName,
+            "collections": db.getSiblingDB(dbName)
+                            .getCollectionInfos(...listColOpts)
+                            .map(collection => ({
+                                "name": collection.name,
+                                "documents": db.getSiblingDB(dbName)
+                                                .getCollection(collection.name)
+                                                .stats().count,
+                                "indexes": db.getSiblingDB(dbName)
+                                                .getCollection(collection.name)
+                                                .getIndexes(),
+                                "$sample": db.getSiblingDB(dbName)
+                                                .getCollection(collection.name)
+                                                .aggregate(pipeline)
+                                                .toArray()
+                            })
+                        ),
+            "views": db.getSiblingDB(dbName)
+                        .getCollectionInfos(...listViewOpts)
+                        .map(view => ({
+                            "name": view.name,
+                            "options": view.options,
+                            "$sample": db.getSiblingDB(dbName)
+                                        .getCollection(view.name)
+                                        .aggregate(pipeline)
+                                        .toArray()
+                            })
+                        )
+        })
+    );
+}
+
+function genReport(schema) {
+    /*
+     *  report
+     */
+    print('\n');
+    print(JSON.stringify(schema, null, '  '));
+    print('\n');
+
+    return;
+}
+
+main(userOptions);
 
 // EOF
