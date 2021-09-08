17bcff4e8ae3ca3cbf45aa3aee8bd09176c1236b
tap1r <luke.prochazka@gmail.com>
2021-09-09 08:53:02 +1000

    Update srvatlas.sh

--- src/srvatlas.sh
+++ src/srvatlas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Name: "srvatlas.sh"
-# Version: "0.1.1"
+# Version: "0.1.2"
 # Description: Atlas cluster name validator
 # Authors: ["tap1r <luke.prochazka@gmail.com>"]
 
@@ -12,8 +12,8 @@ echo -e "Validating Atlas cluster name:\t${_domain}"
 echo -e "TXT resource record:\t\t$_txt"
 
 for _srv in $(dig +short _mongodb._tcp.${_domain} SRV); do
-  if [ "${_srv: -13}" = ".mongodb.net." ]; then
-    echo -e "SRV resource record:\t\t${_srv}"
-    echo -e "Resolves to CNAME/A record:\t$(dig +short ${_srv} A)"
-  fi
+    if [ "${_srv: -13}" = ".mongodb.net." ]; then
+        echo -e "SRV resource record:\t\t${_srv}"
+        echo -e "Resolves to CNAME/A record:\t$(dig +short ${_srv} A)"
+    fi
 done
