cb07b9d16d2c56cbf89093fad7fffb963721eb04
tap1r <luke.prochazka@gmail.com>
2021-12-08 09:27:06 +1100

    Update aggregation-template.js

--- src/aggregation-template.js
+++ src/aggregation-template.js
@@ -1,7 +1,9 @@
 /*
  *  Aggregation template with extended options
  */
+
 var dbName = 'database', collName = 'collection',
+    readPref = 'primary',
     // explainPlan = 'executionStats', // ['queryPlanner'|'executionStats'|'allPlansExecution']
     options = {
         "allowDiskUse": true,
@@ -10,10 +12,10 @@ var dbName = 'database', collName = 'collection',
         // "bypassDocumentValidation": false,
         "readConcern": { "level": "local" },
         // "collation": { "locale": "simple" },
-        // "hint": "_id_",
+        // "hint": { "_id": 1 },
         "comment": "My aggregation query",
-        "writeConcern": { "w": "majority", /* j: true, wtimeout: 10000 */ },
-        // "let": {} // Added in MongoDB v5.0
+        "writeConcern": { "w": "majority"/*, "j": true, "wtimeout": 10000 */ },
+        // "let": { } // Added in MongoDB v5.0
     },
     pipeline = [
         {
@@ -21,5 +23,28 @@ var dbName = 'database', collName = 'collection',
         }
     ];
 
+db.getMongo().setReadPref(readPref);
 db.getSiblingDB(dbName).getCollection(collName).aggregate(pipeline, options).forEach(printjson);
 // db.getSiblingDB(dbName).getCollection(collName).explain(explainPlan).aggregate(pipeline, options);
+
+/*
+ *  Aggregation template for ADL SQL queries
+ */
+
+var dbName = 'database', collName = 'collection',
+    options = { "comment": "My ADL $sql query" },
+    sql = `
+        SELECT *
+        FROM ${collName}
+        LIMIT 1
+    `,
+    pipeline = [{
+        "$sql": {
+            "statement": sql,
+            "format": "jdbc",
+            "formatVersion": 1,
+            "dialect": "mongosql",
+        }
+    }];
+
+db.getSiblingDB(dbName).aggregate(pipeline, options).forEach(printjson);
