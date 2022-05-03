3b74d01d17116683e478fefc0f5f1bcec713defb
tap1r <luke.prochazka@gmail.com>
2022-05-03 16:47:21 +1000

    Update srvatlas.sh

--- src/srvatlas.sh
+++ src/srvatlas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Name: "srvatlas.sh"
-# Version: "0.1.3"
+# Version: "0.2.0"
 # Description: Atlas cluster name validator
 # Authors: ["tap1r <luke.prochazka@gmail.com>"]
 
@@ -25,4 +25,37 @@ for _srv in $(dig +short _mongodb._tcp.${_domain} SRV); do
     fi
 done
 
-echo -e "\nDone."
+echo -e "\nDNS test done."
+
+echo -e "\nValidating connectivity to individual nodes"
+
+for _srv in $(dig +short _mongodb._tcp.${_domain} SRV); do
+    if [[ $_srv =~ ^[0-9]+$ ]]; then
+        if ((_srv > 1023)); then
+            _port=$_srv
+        fi
+    fi
+    if [[ ${_srv: -13} == ".mongodb.net." ]]; then
+        echo -e "\r"
+	    echo -e "Self-identifier from db.hello().me response on node ${_srv}:${_port}\t$(mongo --host ${_srv} --port ${_port} --tls --eval "db.hello().me" --quiet)"
+    fi
+done
+
+echo -e "\nConnectivity test done."
+
+echo -e "\nReplica set topology discovery"
+
+for _srv in $(dig +short _mongodb._tcp.${_domain} SRV); do
+    if [[ $_srv =~ ^[0-9]+$ ]]; then
+        if ((_srv > 1023)); then
+            _port=$_srv
+        fi
+    fi
+    if [[ ${_srv: -13} == ".mongodb.net." ]]; then
+        echo -e "\r"
+	    echo -e "Advertised replset name from node ${_srv}:${_port}\t$(mongo --host ${_srv} --port ${_port} --tls --eval "db.hello().setName" --quiet)"
+        echo -e "Advertised replset hosts from node ${_srv}:${_port}\t$(mongo --host ${_srv} --port ${_port} --tls --eval "db.hello().hosts" --quiet)"
+    fi
+done
+
+echo -e "\nConnectivity test done."
