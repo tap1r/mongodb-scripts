785e37b1ae02ca2488231338bead21cf7066c6b3
tap1r <luke.prochazka@gmail.com>
2022-05-16 12:06:17 +1000

    Update aggregation-template.js
    added $currentOp template

--- src/aggregation-template.js
+++ src/aggregation-template.js
@@ -2,9 +2,9 @@
  *  Aggregation template with extended options
  */
 
-var dbName = 'database', collName = 'collection',
+var dbName = 'database',
+    collName = 'collection',
     readPref = 'primary',
-    // explainPlan = 'executionStats', // ['queryPlanner'|'executionStats'|'allPlansExecution']
     options = {
         "allowDiskUse": true,
         "cursor": { "batchSize": 0 },
@@ -25,7 +25,34 @@ var dbName = 'database', collName = 'collection',
 
 db.getMongo().setReadPref(readPref);
 db.getSiblingDB(dbName).getCollection(collName).aggregate(pipeline, options).forEach(printjson);
-// db.getSiblingDB(dbName).getCollection(collName).explain(explainPlan).aggregate(pipeline, options);
+
+/*
+ *  Aggregation template with explain options
+ */
+
+var dbName = 'database', collName = 'collection',
+    readPref = 'primary',
+    explainPlan = 'executionStats', // ['queryPlanner'|'executionStats'|'allPlansExecution']
+    options = {
+        "allowDiskUse": true,
+        "cursor": { "batchSize": 0 },
+        // "maxTimeMS": 0,
+        // "bypassDocumentValidation": false,
+        "readConcern": { "level": "local" },
+        // "collation": { "locale": "simple" },
+        // "hint": { "_id": 1 },
+        "comment": "Explain my aggregation query",
+        "writeConcern": { "w": "majority"/*, "j": true, "wtimeout": 10000 */ },
+        // "let": { } // Added in MongoDB v5.0
+    },
+    pipeline = [
+        {
+            // aggregation stage operators
+        }
+    ];
+
+db.getMongo().setReadPref(readPref);
+db.getSiblingDB(dbName).getCollection(collName).explain(explainPlan).aggregate(pipeline, options);
 
 /*
  *  Aggregation template for ADL SQL queries
@@ -36,7 +63,7 @@ var dbName = 'database', collName = 'collection',
     sql = `
         SELECT *
         FROM ${collName}
-        LIMIT 1
+        LIMIT 1;
     `,
     pipeline = [{
         "$sql": {
@@ -48,3 +75,35 @@ var dbName = 'database', collName = 'collection',
     }];
 
 db.getSiblingDB(dbName).aggregate(pipeline, options).forEach(printjson);
+
+// Aggregation template for $currentOp
+
+var options = {
+        "cursor": { "batchSize": 0 },
+        "comment": "$currentOp command template",
+        // "let": { } // v5.0+ only
+    },
+    pipeline = [
+        { "$currentOp": {  // default values
+            "allUsers": false,
+            "idleConnections": false,
+            "idleCursors": false,
+            "idleSessions": true,
+            "localOps": false, // mongos only
+            "backtrace": false
+        } },
+        { "$match": {
+            "active": true,
+            // "op": "command",
+            // "ns": { "$regex": '^' + dbName + '.' },
+            // "command.validate": { "$exists": true }
+        } },
+        /* { "$group": {
+            "_id": null
+        } }, */
+        /* { "$project": {
+            "_id": 1 
+        } } */
+    ];
+
+db.getSiblingDB('admin').aggregate(pipeline, options).forEach(printjson);
