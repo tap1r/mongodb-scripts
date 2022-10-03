0b28585cb551cfd57f716b27146ca6cd91c3c75c
tap1r <luke.prochazka@gmail.com>
2022-10-03 11:06:55 +1100

    Update latency.js

--- src/latency.js
+++ src/latency.js
@@ -1,6 +1,6 @@
 /*
  *  Name: "latency.js"
- *  Version: "0.1.1"
+ *  Version: "0.1.2"
  *  Description: driver and network latency telemetry PoC
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  */
@@ -39,30 +39,30 @@ t1 = process.hrtime(t0);
 let { t,
       "attr": { "command": { "$clusterTime": { "clusterTime": clusterTime } } },
       "attr": { "durationMillis": durationMillis }
-} = db.adminCommand(
+    } = db.adminCommand(
         { "getLog": "global" }
     ).log.map(logString => {
         return EJSON.parse(logString)
     }).filter(log => {
         return log?.attr?.command?.comment == filter
     })[0];
-totalTime = t1[0] * 1000 + t1[1] / 1000000;
+totalTime = t1[0] * 1000 + (t1[1] / 1000000);
 t2 = process.hrtime();
 try { (db.hello().ok) }
 catch(e) { printjson(e) }
 t3 = process.hrtime(t2);
-rtt = t3[0] * 1000 + t3[1] / 1000000;
+rtt = t3[0] * 1000 + (t3[1] / 1000000);
 
 console.log(`
-==================================
-Delay/slowms factor:    ${slowms}ms
-Total query time:       ${durationMillis}ms
-Total App/network time: ${(totalTime - durationMillis).toFixed(2)}ms
-----------------------------------
-Total time:             ${totalTime.toFixed(2)}ms
-----------------------------------
-Server execution time:  ${durationMillis - slowms}ms
-Network Latency (RTT):  ${rtt.toFixed(2)}ms
-Driver execution time:  ${(totalTime - durationMillis - rtt).toFixed(2)}ms
-==================================
+    ===================================
+    Delay/slowms factor:\t${slowms}ms
+    Total query time:\t\t${durationMillis}ms
+    Total App/network time:\t${(totalTime - durationMillis).toFixed(2)}ms
+    -----------------------------------
+    Total measurement time:\t${totalTime.toFixed(2)}ms
+    -----------------------------------
+    Server execution time:\t${durationMillis - slowms}ms
+    Network Latency (RTT):\t${rtt.toFixed(2)}ms
+    Driver execution time:\t${(totalTime - durationMillis - rtt).toFixed(2)}ms
+    ===================================
 `);
