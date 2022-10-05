02454f0cbc9412508c84156f1db400e76c40e716
tap1r <luke.prochazka@gmail.com>
2022-10-05 16:28:14 +1100

    Update srvatlas.sh

--- src/srvatlas.sh
+++ src/srvatlas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Name: "srvatlas.sh"
-# Version: "0.3.8"
+# Version: "0.3.9"
 # Description: Atlas/SRV cluster name/connection validator
 # Authors: ["tap1r <luke.prochazka@gmail.com>"]
 
@@ -60,8 +60,8 @@ fi
 }
 
 # verify if the supplied cluster-name is valid
-_txt=$($_lookupCmd -r +short $_clusterName TXT)
-_a=$($_lookupCmd -r +short $_clusterName A)
+_txt=$($_lookupCmd +short $_clusterName TXT)
+_a=$($_lookupCmd +short $_clusterName A)
 [ ! -z $_txt ] || {
     echo -e "ERROR: TXT lookup failed for $_clusterName, is it a valid cluster name?"
     exit 1
@@ -78,22 +78,22 @@ while IFS=' ' read -a line; do
     for ((n=0; n<${#line[@]}; n+=4)); do
         _host=${line[$n+3]}
         _port=${line[$n+2]}
-        _resolved=$($_lookupCmd -r +short $_host A)
+        _resolved=$($_lookupCmd +short $_host A)
         echo -e "\n\tSRV resource record:\t$_host"
         echo -e "\tResolves to CNAME/A:\t${_resolved//$'\n'/ / }"
         echo -e "\tService parameter:\tTCP/$_port"
         _targets+=(${_host%\.}:${_port})
     done
     break
-done <<< $($_lookupCmd -r +short _mongodb._tcp.$_clusterName SRV)
+done <<< $($_lookupCmd +short _mongodb._tcp.$_clusterName SRV)
 
 # measure DNS latency of batched lookups
 echo -e "\nDNS query latency:\n"
 _queryRegex="Query time\: ([0-9]*) msec"
 _totalQuery=0
 _batchLatency=0
-_txtQuery=$($_lookupCmd -r +stats $_clusterName TXT &)
-_srvQuery=$($_lookupCmd -r +stats _mongodb._tcp.$_clusterName SRV &)
+_txtQuery=$($_lookupCmd +stats $_clusterName TXT &)
+_srvQuery=$($_lookupCmd +stats _mongodb._tcp.$_clusterName SRV &)
 wait
 
 [[ ${_txtQuery} =~ $_queryRegex ]] && {
@@ -109,7 +109,7 @@ wait
 while IFS=' ' read -a line; do
     for ((n=0; n<${#line[@]}; n+=4)); do
         _host=${line[$n+3]}
-        _hostQuery=$($_lookupCmd -r +stats ${_host} A)
+        _hostQuery=$($_lookupCmd +stats ${_host} A)
         [[ ${_hostQuery} =~ $_queryRegex ]] && {
             # echo -e "\tA query latency:\t${BASH_REMATCH[1]}ms"
             let "_totalQuery+=${BASH_REMATCH[1]}"
@@ -117,7 +117,7 @@ while IFS=' ' read -a line; do
         }
     done
     break
-done <<< $($_lookupCmd -r +short _mongodb._tcp.$_clusterName SRV)
+done <<< $($_lookupCmd +short _mongodb._tcp.$_clusterName SRV)
 
 # echo -e "\tA query latency:\t${_aLookups[@]} ms"
 IFS=$'\n'
@@ -197,13 +197,16 @@ for _target in "${_targets[@]}"; do {
     echo -e "\tmaxWireVersion:\t\t$_maxWireVersion"
     echo -e "\tTLS cipher scanning:";
     for _suite in ${_cipherSuites[@]}; do {
-        _ciphers="None"
-        for _cipher in $($_openssl ciphers -s -$_suite -ciphersuites $_tls1_3_suites $_policy | tr ':' ' '); do
-            timeout $_connectTimeout $_openssl s_client -connect "$_target" -cipher $_cipher -$_suite -async < /dev/null > /dev/null 2>&1 && _ciphers+=($_cipher)
+        _negotiatedCiphers="None"
+        # for _cipher in $($_openssl ciphers -s -$_suite -ciphersuites $_tls1_3_suites $_policy | tr ':' ' '); do
+        _ciphers=$($_openssl ciphers -s -$_suite -ciphersuites $_tls1_3_suites $_policy | tr ':' ' ')
+        # for _cipher in ${$_ciphers//\:/ }; do
+        for _cipher in ${_ciphers}; do
+            timeout $_connectTimeout $_openssl s_client -connect "$_target" -cipher $_cipher -$_suite -async < /dev/null > /dev/null 2>&1 && _negotiatedCiphers+=($_cipher)
         done
-        [[ ${#_ciphers[@]} -gt 1 ]] && unset _ciphers[0]
-        echo -e "\n\t\t$_suite: ${_ciphers[@]}"
-        unset _ciphers
+        [[ ${#_negotiatedCiphers[@]} -gt 1 ]] && unset _negotiatedCiphers[0]
+        echo -e "\n\t\t$_suite: ${_negotiatedCiphers[@]}"
+        unset _negotiatedCiphers
     } # &
     done
     # wait
