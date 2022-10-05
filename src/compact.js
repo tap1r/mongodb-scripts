808bb64e024e6eb5559448a5f2c9fda5a6822ed3
tap1r <luke.prochazka@gmail.com>
2022-10-05 11:33:55 +1100

    Update compact.js

--- src/compact.js
+++ src/compact.js
@@ -1,65 +1,88 @@
 /*
  *  Name: "compact.js"
- *  Version = "0.1.2"
+ *  Version: "0.2.0"
  *  Description: schrödinger's page reproduction
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  */
 
-var dbName = 'database',
+let dbName = 'database',
     collName = 'collection',
-    n = 75,
-    deleteFilter = {
-        "$expr": {
-            "$gt": [n / 100, { "$rand": {} }]
-        }
-    },
-    updateFilter = {}, // all docs
-    setOptions = [{
-        "$set": {
-            "_$$compaction": {
-                "status": "modified",
-                "lastModified": "$$NOW",
-                "comment": "added by compact.js"
-            }
-        }
-    }],
-    unsetOptions = [{
-        "$unset": "_$$compaction"
-    }];
+    n = 25, // = % chance of being matched
+    rounds = 5, // iterations of entropy
+    randFilter = { "$expr": { "$gt": [n / 100, { "$rand": {} }] } },
+    // update = { "$set": { "x": Math.random() } },
+    compactions = 1;
 
-for (let i = 1; i < 4; ++i) {
+let namespace = db.getSiblingDB(dbName).getCollection(collName);
+
+for (let i = 1; i <= rounds; ++i) {
     /*
      *  generate dataset with increased entropy
      */
-    print('Round', i);
-    print('Generating data');
+    console.log(`\nRound ${i} of ${rounds}:\tGenerating data`);
     load('fuzzer.js');
-    print('Pruning data');
-    try { // delete n% of existing documents
-        db.getSiblingDB(dbName).getCollection(collName).deleteMany(deleteFilter);
-    } catch(e) {
-        print(e);
-    }
+    console.log(`\tPruning data`);
+    // delete n% of existing documents
+    try { namespace.deleteMany(randFilter) }
+    catch(e) { console.log(e) }
+    /* console.log(`\tUpdating data`);
+    try { namespace.updateMany(randFilter, update) }
+    catch(e) { console.log(e) } */
 }
+
+// Report initial dbStats
+
+console.log('Gathering initial dbStats');
+load('dbstats.js');
 
 // "touch" documents to force page re-writes
 
-print('Setting');
-try {
-    db.getSiblingDB(dbName).getCollection(collName).updateMany(updateFilter, setOptions);
-  } catch(e) {
-    print(e);
-}
+/*
+console.log(`Setting`);
+try { namespace.updateMany(updateFilter, setOptions) }
+catch(e) { print(e) }
 
-print('Unsetting');
-try {
-    db.getSiblingDB(dbName).getCollection(collName).updateMany(updateFilter, unsetOptions);
-} catch(e) {
-    print(e);
-}
+console.log(`Unsetting`);
+try { namespace.updateMany(updateFilter, unsetOptions) }
+catch(e) { print(e) }
+*/
+
+// Report dbStats pre-compaction
+
+console.log(`Gathering pre-compaction dbStats`);
+load('dbstats.js');
 
 // compact()
-print('Compacting collection');
-db.getSiblingDB(dbName).runCommand({ "compact": collName });
+for (let i = 1; i <= compactions; ++i) {    
+    console.log(`Compacting collection ${i} of ${compactions}`);
+    db.getSiblingDB(dbName).runCommand({ "compact": collName });
+
+    /*
+        db.getSiblingDB('admin').aggregate([
+            { "$currentOp": {} },
+            { "$match": {
+                "active": true,
+                "op": "command",
+                "command.compact": { "$exists": true }
+            } }
+        ]).forEach(op =>
+            console.log(`\nCurrently compacting namespace: ${op.command['$db']}.${op.command.compact}`)
+        );
+
+        const watchCursor = db.getMongo().watch([{ "$match": {} }]);
+        while (!watchCursor.isClosed()) {
+            let next = watchCursor.tryNext();
+            while (next !== null) {
+            printjson(next);
+            next = watchCursor.tryNext();
+            }
+        }
+    */
+}
+
+// Report final dbStats post-compaction
+
+console.log('Gathering post-compaction dbStats');
+load('dbstats.js');
 
 // EOF
