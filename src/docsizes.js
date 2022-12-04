6f93175b436401766079eb779350179b125271bc
tap1r <luke.prochazka@gmail.com>
2022-12-05 10:38:19 +1100

    Update docsizes.js

--- src/docsizes.js
+++ src/docsizes.js
@@ -1,13 +1,13 @@
 /*
- *  Name: "docsizes.js"
+ *  Name: "docSizes.js"
  *  Version: "0.1.8"
  *  Description: sample document size distribution
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  */
 
-// Usage: "mongosh [connection options] --quiet docsizes.js"
+// Usage: "mongosh [connection options] --quiet docSizes.js"
 
-let __script = { "name": "docsizes.js", "version": "0.1.8" };
+let __script = { "name": "docSizes.js", "version": "0.1.8" };
 console.log(`\n---> Running script ${__script.name} v${__script.version}\n`);
 
 /*
@@ -17,13 +17,15 @@ console.log(`\n---> Running script ${__script.name} v${__script.version}\n`);
 let options = {
    "dbName": "database",
    "collName": "collection",
-   // "sampleSize": 1000,  // parameter to $sample
+   // "sampleSize": 1000,
+   // "buckets": [],
+   // "pages": []
 };
 
 (({
       dbName,
       collName,
-      sampleSize = 1000,
+      sampleSize = 1000,   // parameter to $sample
       buckets = Array.from([0, 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384], block => block * 1024),
       pages = Array.from([0, 1, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384], block => block * 1024)
    }) => {
@@ -36,9 +38,9 @@ let options = {
       ? 'primaryPreferred'
       : 'secondaryPreferred';
    db.getMongo().setReadPref(readPref);
-   
-   let namespace = db.getSiblingDB(dbName).getCollection(collName),
-      {
+
+   let namespace = db.getSiblingDB(dbName).getCollection(collName);
+   let {
          'size': dataSize,
          'wiredTiger': {
             'block-manager': {
@@ -59,7 +61,7 @@ let options = {
       { 'system': { hostname } } = db.hostInfo(),
       { 'parsed': { 'storage': { dbPath } } } = db.serverCmdLineOpts(),
       overhead = 0, // 2 * 1024 * 1024;
-      pageSize = 32768;
+      pageSize = 32768,
       compressor = creationString.match(/block_compressor=(?<compressor>\w+)/).groups.compressor,
       ratio = +(dataSize / (storageSize - blocksFree - overhead)).toFixed(2);
 
@@ -116,8 +118,8 @@ let options = {
             "compressor": compressor,
             "compressionRatio": ratio,
             "documentCount": documentCount,
-            "consumed32kPages": Math.ceil((storageSize - blocksFree - overhead)/pageSize),
-            "avgDocsPer32kPage": +(documentCount/((storageSize - blocksFree - overhead)/pageSize)).toFixed(2)
+            "consumed32kPages": Math.ceil((storageSize - blocksFree - overhead) / pageSize),
+            "avgDocsPer32kPage": +(documentCount/((storageSize - blocksFree - overhead) / pageSize)).toFixed(2)
       } } }
    ];
 
