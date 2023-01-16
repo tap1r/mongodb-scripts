6aae581d6c21736f8a316cdb0423563c9dfcd700
tap1r <luke.prochazka@gmail.com>
2023-01-16 14:38:40 +1100

    Update schema-sampler.js

--- src/schema-sampler.js
+++ src/schema-sampler.js
@@ -1,14 +1,15 @@
 /*
  *  Name: "schema-sampler.js"
- *  Version: "0.2.4"
+ *  Version: "0.2.5"
  *  Description: generate schema with simulated mongosqld sampling commands
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  */
 
 // Usage: "[mongo|mongosh] [connection options] --quiet schema-sampler.js > schema.json"
 
-let userOptions = {  // user preferences
-   // "sampleSize": 1, // defaults to 1 for performance reasons, increase for sparse data
+// user preferences
+let userOptions = {
+   "sampleSize": 10, // defaults to 1 for performance reasons, increase for sparse data
    // "dbs": ['namespace'], // restrict list to known namespaces
    // "readPreference": 'secondaryPreferred'
 };
@@ -17,8 +18,8 @@ let userOptions = {  // user preferences
    /*
     *
     */
-   const __script = { "name": "schema-sampler.js", "version": "0.2.4" };
-   print(`\nRunning script ${__script.name} v${__script.version}`);
+   let __script = { "name": "schema-sampler.js", "version": "0.2.5" };
+   print(`\n---> Running script ${__script.name} v${__script.version}`);
    
    function main({ sampleSize = 1, dbs = [], readPreference = 'secondaryPreferred' }) {
       /*
@@ -36,17 +37,21 @@ let userOptions = {  // user preferences
       /*
        *  generate a synthetic schema with metadata
        */
+      let comment = 'Executed by ' + __script.name + ' v' + __script.version;
       let collectionPipeline = [{ "$sample": { "size": sampleSize } }];
       let viewPipeline = [{ "$sample": { "size": 1} }];
-      // let comment = 'Executed by ' + __script.name + ' v' + __script.version;
+      let options = {
+         "allowDiskUse": true,
+         "cursor": { "batchSize": sampleSize },
+         "readConcern": { "level": "local" },
+         "comment": comment,
+      };
       let listDbOpts = [{
-            "listDatabases": 1,
-            "filter": { "name": /(?:^(?!admin$|config$|local$)).+/ },
-            "nameOnly": true,
-            "authorizedDatabases": true
-         },
-         // comment
-      ];
+         "listDatabases": 1,
+         "filter": { "name": /(?:^(?!admin$|config$|local$)).+/ },
+         "nameOnly": true,
+         "authorizedDatabases": true
+      }];
       // db.runCommand({ "listCollections": 1, "authorizedCollections": true, "nameOnly": true });
       let listColOpts = [{
             "type": "collection",
@@ -77,17 +82,17 @@ let userOptions = {  // user preferences
                                                    .getIndexes(),
                                  "$sample":      db.getSiblingDB(dbName)
                                                    .getCollection(collection.name)
-                                                   .aggregate(collectionPipeline)
+                                                   .aggregate(collectionPipeline, options)
                                                    .toArray()
             })),
             "views": db.getSiblingDB(dbName)
-                     .getCollectionInfos(...listViewOpts)
-                     .map(view => ({
+                       .getCollectionInfos(...listViewOpts)
+                       .map(view => ({
                            "name":     view.name,
                            "options":  view.options,
                            "$sample":  db.getSiblingDB(dbName)
                                          .getCollection(view.name)
-                                         .aggregate(viewPipeline)
+                                         .aggregate(viewPipeline, options)
                                          .toArray()
             }))
       }))
