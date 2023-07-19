d9341623f568bffa26c00c9d8d99667ab9619787
tap1r <luke.prochazka@gmail.com>
2023-07-19 15:49:09 +1000

    Update dbstats.js

--- src/dbstats.js
+++ src/dbstats.js
@@ -1,6 +1,6 @@
 /*
  *  Name: "dbstats.js"
- *  Version: "0.5.2"
+ *  Version: "0.5.3"
  *  Description: DB storage stats uber script
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  */
@@ -35,7 +35,7 @@
  */
 
 (async() => {
-   let __script = { "name": "dbstats.js", "version": "0.5.2" };
+   let __script = { "name": "dbstats.js", "version": "0.5.3" };
    if (typeof __lib === 'undefined') {
       /*
        *  Load helper library mdblib.js
@@ -58,8 +58,8 @@
    __comment += ` on shell v${version()}`;
    console.clear();
    console.log(`\u001b[32m${__comment}\u001b[0m`);
-   if (shellVer() < serverVer() && typeof process === 'undefined') console.log(`\u001b[31m[WARN] Possible incompatible shell version detected: ${shellVer()}\u001b[0m`);
-   if (serverVer() < 4.2) console.log(`\u001b[31m[ERROR] Unsupported mongod/s version detected: ${serverVer()}\u001b[0m`);
+   if (shellVer() < serverVer() && typeof process === 'undefined') console.log(`\n\u001b[31m[WARN] Possible incompatible shell version detected: ${shellVer()}\u001b[0m`);
+   if (serverVer() < 4.2) console.log(`\n\u001b[31m[ERROR] Unsupported mongod/s version detected: ${serverVer()}\u001b[0m`);
 
    /*
     *  User defined parameters
