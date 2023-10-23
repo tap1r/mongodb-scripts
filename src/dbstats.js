39723b3b2a9bf4e5ecbba9c97970dbde1ea3e316
tap1r <luke.prochazka@gmail.com>
2023-10-23 11:58:27 +1100

    Update dbstats.js

--- src/dbstats.js
+++ src/dbstats.js
@@ -1,6 +1,6 @@
 /*
  *  Name: "dbstats.js"
- *  Version: "0.5.8"
+ *  Version: "0.5.9"
  *  Description: DB storage stats uber script
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  */
@@ -36,7 +36,7 @@
  */
 
 (async(dbFilter, collFilter) => {
-   let __script = { "name": "dbstats.js", "version": "0.5.8" };
+   let __script = { "name": "dbstats.js", "version": "0.5.9" };
    if (typeof __lib === 'undefined') {
       /*
        *  Load helper library mdblib.js
@@ -60,7 +60,7 @@
    console.clear();
    console.log(`\u001b[33m${__comment}\u001b[0m`);
    if (shellVer() < serverVer() && typeof process === 'undefined') console.log(`\n\u001b[31m[WARN] Possible incompatible shell version detected: ${shellVer()}\u001b[0m`);
-   if (shellVer() < 1.0 && typeof process === 'object') console.log(`\n\u001b[31m[WARN] Possible incompatible non-GA shell version detected: ${shellVer()}\u001b[0m`);
+   if (shellVer() < 1.0 && typeof process !== 'undefined') console.log(`\n\u001b[31m[WARN] Possible incompatible non-GA shell version detected: ${shellVer()}\u001b[0m`);
    if (serverVer() < 4.2) console.log(`\n\u001b[31m[ERROR] Unsupported mongod/s version detected: ${serverVer()}\u001b[0m`);
 
    /*
