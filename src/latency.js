ad00a41e1be42825d46937ebc085f06dfe93fbf7
tap1r <luke.prochazka@gmail.com>
2023-10-23 14:07:28 +1100

    Update latency.js

--- src/latency.js
+++ src/latency.js
@@ -1,6 +1,6 @@
 /*
  *  Name: "latency.js"
- *  Version: "0.2.4"
+ *  Version: "0.2.5"
  *  Description: driver and network latency telemetry PoC
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  */
@@ -13,16 +13,16 @@ console.clear();
    /*
     *  main
     */
-   let __script = { "name": "latency.js", "version": "0.2.4" };
-   console.log(`\n---> Running script ${__script.name} v${__script.version}\n`);
+   let __script = { "name": "latency.js", "version": "0.2.5" };
+   console.log(`\n\u001b[33m---> Running script ${__script.name} v${__script.version} on shell v${version()}\u001b[0m\n`);
 
    let slowms = 100,
       filter = `Synthetic slow operation at ${performance.now()}`;
    try {
       slowms = db.getSiblingDB('admin').getProfilingStatus().slowms;
    } catch(error) {
-      console.log('[WARN] failed to aquire the slowms threshold:', error);
-      console.log('[WARN] defaulting slowms to 200ms');
+      console.log('\u001b[31m[WARN] failed to aquire the slowms threshold:\u001b[0m', error);
+      console.log('\u001b[31m[WARN] defaulting slowms to 200ms\u001b[0m');
       slowms = 200;
    }
    // let { slowms = 100 } = db.getSiblingDB('admin').getProfilingStatus();
@@ -71,12 +71,12 @@ console.clear();
       t3 = process.hrtime(t2);
       if (!ok) throw new Error();
    } catch(error) {
-      console.log('SDAM ping failed');
+      console.error('SDAM ping failed');
       throw error;
    }
 
-   totalTime = t1[0] * 1000 + (t1[1] / 1000000);
-   rtt = t3[0] * 1000 + (t3[1] / 1000000);
+   totalTime = t1[0] * 1000 + (t1[1] / 1000000.0);
+   rtt = t3[0] * 1000 + (t3[1] / 1000000.0);
 
    function fomatted(duration) {
       return Intl.NumberFormat('en', {
@@ -93,18 +93,18 @@ console.clear();
    tableWidth = columnWidth + longestValue + spacing;
    padding = longestValue + spacing;
    report = `
-   Measurement
-   ${'-'.repeat(tableWidth)}
-   ${'Delay/slowms factor:'.padEnd(columnWidth)}${fomatted(slowms).padStart(padding)}
-   ${'Total measurement time:'.padEnd(columnWidth)}${fomatted(totalTime).padStart(padding)}
-   ${'='.repeat(tableWidth)}
+   \u001b[1mMeasurement\u001b[0m
+   \u001b[33m${'-'.repeat(tableWidth)}\u001b[0m
+   \u001b[32m${'Delay/slowms factor:'.padEnd(columnWidth)}\u001b[0m${fomatted(slowms).padStart(padding)}
+   \u001b[32m${'Total measurement time:'.padEnd(columnWidth)}\u001b[0m${fomatted(totalTime).padStart(padding)}
+   \u001b[33m${'='.repeat(tableWidth)}\u001b[0m
 
-   Latency breakdown
-   ${'-'.repeat(tableWidth)}
-   ${'Server execution time:'.padEnd(columnWidth)}${fomatted(durationMillis - slowms).padStart(padding)}
-   ${'Network latency (RTT):'.padEnd(columnWidth)}${fomatted(rtt).padStart(padding)}
-   ${'Driver execution time:'.padEnd(columnWidth)}${fomatted(totalTime - durationMillis - rtt).padStart(padding)}
-   ${'='.repeat(tableWidth)}
+   \u001b[1mLatency breakdown\u001b[0m
+   \u001b[33m${'-'.repeat(tableWidth)}\u001b[0m
+   \u001b[32m${'Server execution time:'.padEnd(columnWidth)}\u001b[0m${fomatted(durationMillis - slowms).padStart(padding)}
+   \u001b[32m${'Network latency (RTT):'.padEnd(columnWidth)}\u001b[0m${fomatted(rtt).padStart(padding)}
+   \u001b[32m${'Driver execution time:'.padEnd(columnWidth)}\u001b[0m${fomatted(totalTime - durationMillis - rtt).padStart(padding)}
+   \u001b[33m${'='.repeat(tableWidth)}\u001b[0m
    `;
    console.log(report);
 })();
