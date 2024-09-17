41870c8029fc562a1c5e52e0500e88bcac945672
Luke Prochazka <luke.prochazka@gmail.com>
2024-09-17 13:52:02 +1000

    Create connStats.js

---
+++ src/connStats.js
@@ -0,0 +1,217 @@
+(() => {
+   /*
+    *  Name: "connStats.js"
+    *  Version: "0.1.0"
+    *  Description: "report detailed connection pooling statistics"
+    *  Disclaimer: https://raw.githubusercontent.com/tap1r/mongodb-scripts/master/DISCLAIMER.md
+    *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
+    */
+   let aggOpts = {
+         "comment": "connStats.js v0.1.0"
+      },
+      pipeline = [
+         { "$currentOp": {
+            "allUsers": true,
+            "localOps": true,
+            "idleConnections": true,
+            "idleCursors": true,
+            "idleSessions": true
+            // "targetAllNodes": true // sharded option
+         } },
+         { "$match": {
+            "client": { "$exists": true } // minimum requirement to capture network client details
+            // use post match filter for any other criteria to avoid bypassing the pool matching heuristics
+         } },
+         { "$group": {
+            "_id": {
+               "host": "$host",
+               "client": { // minimum requirement to detect distinct client pools
+                  "endpoint": { "$first": { "$split": ["$client", ":"] } },
+                  "driverVersion": "$clientMetadata.driver.version",
+                  "platform": "$clientMetadata.platform",
+                  "os": "$clientMetadata.os"
+                  // do not use application or driver names here
+                  // they can vary on SDAM connections within the same MongoClient() instance
+            } },
+            "connections": {
+               "$push": {
+                  "applicationName": { "$ifNull": ["$clientMetadata.application.name", "$clientMetadata.driver.name"] },
+                  "connectionId": { "$ifNull": ["$connectionId", null] },
+                  "ephemeralPort": { "$toInt": { "$arrayElemAt": [{ "$split": ["$client", ":"] }, 1] } },
+                  "opid": { "$ifNull": ["$opid", null] },
+                  // "lsid": { "$ifNull": ["$lsid.id", null] },
+                  "opType": { "$ifNull": ["$op", null] },
+                  "msg": { "$ifNull": ["$msg", null] },
+                  "active": "$active",
+                  // "currentOpTime": { "$toDate": "$currentOpTime" },
+                  "secs_running": { "$ifNull": ["$secs_running", null] },
+                  "microsecs_running": { "$ifNull": ["$microsecs_running", null] },
+                  "command": { "$ifNull": ["$command", null] },
+                  "sdam": { // streaming hello monitor
+                     "$and": [
+                        { "$or": [
+                           { "$eq": ["$clientMetadata.driver.name", "NetworkInterfaceTL"] },
+                           "$command.hello",
+                           "$command.isMaster",
+                           "$command.ismaster"
+                        ] },
+                        "$command.maxAwaitTimeMS",
+                        { "$not": { "$ifNull": ["$effectiveUsers.user", false] } }
+                  ] },
+                  "rtt": { // rtt monitor
+                     "$and": [
+                        { "$or": [
+                           { "$eq": ["$clientMetadata.driver.name", "NetworkInterfaceTL"] },
+                           "$command.hello",
+                           "$command.isMaster",
+                           "$command.ismaster",
+                           "$command.ping",
+                           { "$not": { "$ifNull": ["$command", false] } }
+                        ] },
+                        { "$not": { "$ifNull": ["$command.maxAwaitTimeMS", false] } },
+                        { "$not": { "$ifNull": ["$effectiveUsers.user", false] } }
+                  ] },
+                  // "namespace": { "$ifNull": ["$ns", null] },
+                  "user": { // reconstitute the user format
+                     "$ifNull": [
+                        { "$concat": [
+                           { "$first": "$effectiveUsers.user" },
+                           "@",
+                           { "$first": "$effectiveUsers.db" }
+                        ] },
+                        "unprivileged"
+                  ] }
+            } }
+         } },
+         { "$set": {
+            "host": "$_id.host",
+            "appName": {
+               "$max": {
+                  "$filter": {
+                     "input": "$connections.applicationName",
+                     "as": "appName",
+                     "cond": { "$ne": ["NetworkInterfaceTL", "$$appName"] }
+            } } },
+            "srcIP": "$_id.client.endpoint",
+            "driverVersion": "$_id.client.driverVersion",
+            "platform": "$_id.client.platform",
+            "os": "$_id.client.os",
+            // "user": { "$min": "$connections.user" },
+            "authenticatedUsers": {
+               "$setDifference": [
+                  { "$setIntersection": [
+                     "$connections.user",
+                     "$connections.user"
+                  ] },
+                  ["unprivileged"]
+            ] },
+            // "users": "$connections.user",
+            "activePooledConnections": {
+               "$sum": {
+                  "$map": {
+                     "input": "$connections",
+                     "as": "connection",
+                     "in": {
+                        "$cond": [
+                           { "$and": [
+                              { "$not": {
+                                 "$or": [
+                                    "$$connection.sdam",
+                                    "$$connection.rtt"
+                              ] } },
+                              "$$connection.active"
+                           ] },
+                           1, 0
+            ] } } } },
+            "idlePooledConnections": {
+               "$sum": {
+                  "$map": {
+                     "input": "$connections",
+                     "as": "connection",
+                     "in": {
+                        "$cond": [
+                           { "$and": [
+                              { "$not": {
+                                 "$or": [
+                                    "$$connection.sdam",
+                                    "$$connection.rtt"
+                              ] } },
+                              { "$not": "$$connection.active" }
+                           ] },
+                           1, 0
+            ] } } } },
+            "adminConnections": { // administrative/monitoring connections
+               "$sum": {
+                  "$map": {
+                     "input": "$connections",
+                     "as": "connection",
+                     "in": {
+                        "$cond": [
+                           { "$or": [
+                              "$$connection.sdam",
+                              "$$connection.rtt"
+                           ] },
+                           1, 0
+            ] } } } },
+            "pools": { // indicative of distinct MongoClient() instances
+               "$sum": {
+                  "$map": {
+                     "input": "$connections.sdam",
+                     "as": "sdam",
+                     "in": { "$cond": ["$$sdam", 1, 0] }
+            } } },
+            "MongoClientOpids": {
+               "$map": {
+                  "input": {
+                     "$filter": {
+                        "input": "$connections",
+                        "as": "connection",
+                        "cond": "$$connection.sdam"
+                     } },
+                  "as": "sdam",
+                  "in": "$$sdam.opid"
+            } },
+            "sdamConnectionIds": {
+               "$map": {
+                  "input": {
+                     "$filter": {
+                        "input": "$connections",
+                        "as": "connection",
+                        "cond": "$$connection.sdam"
+                     } },
+                  "as": "sdam",
+                  "in": "$$sdam.connectionId"
+            } },
+            "rttConnectionIds": {
+               "$map": {
+                  "input": {
+                     "$filter": {
+                        "input": "$connections",
+                        "as": "connection",
+                        "cond": "$$connection.rtt"
+                     } },
+                  "as": "rtt",
+                  "in": "$$rtt.connectionId"
+            } },
+            "totalConnections": { "$size": "$connections" }
+         } },
+         { "$match": { // post filter recommended on dervied pool metrics
+            // "users": { "$in": [{ "user": "tapir", "db": "admin" }, null] }, // use null to capture SDAM events
+            // "appName": { "$in": [/^greedyApp/, null] }, // use null to capture SDAM events
+            // "appName": { "$in": [/^(?:mongosh|MongoDB Shell)/, null] }, // use null to capture SDAM events
+            // "appName": /^(?:nodejs|MongoDB Internal Client|NetworkInterfaceTL)/,
+            // "ns": /^admin/,
+            // "command.aggregate": { "$exists": true },
+            // "secs_running": { "$gte": 0 }
+         } },
+         { "$sort": { "totalConnections": -1 } },
+         { "$project": {
+            "_id": 0,
+            "connections": 0
+         } }
+      ],
+      results;
+
+   results = db.getSiblingDB('admin').aggregate(pipeline, aggOpts).toArray();
+   console.log(results);
+})();
