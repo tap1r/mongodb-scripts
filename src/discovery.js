b2bc45de623ddcd4d112e290eb129a035e0bad3b
Luke Prochazka <luke.prochazka@gmail.com>
2024-10-25 17:04:49 +1100

    Update discovery.js

--- src/discovery.js
+++ src/discovery.js
@@ -1,101 +1,63 @@
 /*
  *  Name: "discovery.js"
- *  Version: "0.1.4"
+ *  Version: "0.1.5"
  *  Description: "topology discovery with directed command execution"
  *  Disclaimer: "https://raw.githubusercontent.com/tap1r/mongodb-scripts/master/DISCLAIMER.md"
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  *
- *  Notes: mongosh only, support for async required to parallelise and access the topology with auth
+ *  Notes:
+ *  - mongosh only
+ *  - support for async required to parallelise and access the topology with auth
  *  TBA:
  *  - contextualise mdblib.js
  */
 
-// Usage: "mongosh [connection options] --quiet discovery.js"
+// Usage: mongosh [connection options] --quiet -f discovery.js
 
 (async() => {
-   async function main() {
+   /*
+    *
+    */
 
-      // load('mdblib.js');
-
-      // ['ReplSet', 'Sharded', 'LoadBalanced', 'Standalone']
-
-      function discoverHosts() {
-         let hosts = [];
-         try {
-            hosts = rs.status().members.map(({ name, health, stateStr }) =>
-               new Object({ 'host': name, 'health': health, 'role': stateStr })
-            ).filter(({ health, role }) =>
-               health == 1 && role != 'ARBITER'
-            );
-         }
-         catch(e) {
-            console.log('Lack the rights to discover hidden nodes:', e);
-            hosts = db.hello().hosts;
-         }
-
-         return hosts;
-      }
-
-      function discoverShards() {
-         let shards = [];
-         try {
-            shards = db.adminCommand({ "listShards": 1 }).shards;
-         }
-         catch(e) {
-            console.log('Lack the ability to discover shards:', e);
-         }
-
-         return shards;
-      }
-
-      async function fetchHostStats({ 'host': hostname }) {
-         let directURI;
-         if (username == null) {
-            directURI = `mongodb://${hostname}/?directConnection=true&tls=${tls}&compressors=${compressors}&readPreference=${readPreference}`;
-         } else {
-            directURI = `mongodb://${username}:${password}@${hostname}/?directConnection=true&tls=${tls}&authSource=${authSource}&authMechanism=${authMech}&compressors=${compressors}&readPreference=${readPreference}`;
-         }
-         let node = connect(directURI);
-         // console.log(getDBNames());
-         // console.log('listDatabases:', node.getSiblingDB('admin').runCommand({ "listDatabases": 1, "nameOnly": false }, { "readPreference": readPreference }).databases);
-         // console.log('dbstats:', node.getSiblingDB('admin').stats());
-         // console.log('listCollections:', node.getSiblingDB('database').runCommand({ "listCollections": 1, "authorizedCollections": true, "nameOnly": true }, { "readPreference": readPreference }).cursor.firstBatch);
-         // console.log('collStats:', node.getSiblingDB('database').getCollection('collection').aggregate({ "$collStats": { "storageStats": { "scale": 1 } } }).toArray()[0].ns);
-
-         let me = async() => node.hello().me;
-         let stats = async() => node.getSiblingDB('admin').runCommand({ "listDatabases": 1, "nameOnly": false }, { "readPreference": readPreference }).databases;
-         let results = {
-            'process': await me(),
-            'stats': await stats()
-         };
-         return results;
-      }
-
-      function discoverShardedHosts(shards) {
-         // shards.forEach(({ _id, host }) => console.log(`\nID: ${_id} with: ${host}`));
-         let hosts = shards.map(({ host }) => {
-            // console.log(`\nConnecting to: ${_id}`);
-            let { setName, seedList } = host.match(/(?<setName>\w+)\/(?<seedList>.+)/).groups;
-            return seedList.split(',');
-         });
-         hosts = hosts.flat();
-         hosts = hosts.map(name =>
-            new Object({ 'host': name, 'health': '', 'role': '' })
+   function discoverRSHosts() {
+      /*
+       *
+       */
+      let hosts = [];
+      try {
+         hosts = rs.status().members.map(({ name, health, stateStr }) =>
+            new Object({ 'host': name, 'health': health, 'role': stateStr })
+         ).filter(({ health, role }) =>
+            health == 1 && role != 'ARBITER'
          );
-         // let promises = shards.map(fetchShardHosts(setName, seedList));
-
-         // return await Promise.all(promises);
-         // return await Promise.allSettled(promises);
-         return hosts;
+      }
+      catch(e) {
+         console.log('Lack the rights to discover hidden nodes:', e);
+         hosts = db.hello().hosts;
       }
 
-      async function fetchAllStats(hosts) {
-         let promises = hosts.map(fetchHostStats);
+      return hosts;
+   }
 
-         return Promise.all(promises);
-         // return await Promise.allSettled(promises);
+   function discoverShards() {
+      /*
+       *
+       */
+      let shards = [];
+      try {
+         shards = db.adminCommand({ "listShards": 1 }).shards;
+      }
+      catch(e) {
+         console.log('Lack the ability to discover shards:', e);
       }
 
+      return shards;
+   }
+
+   async function fetchHostStats({ 'host': hostname } = {}) {
+      /*
+       *
+       */
       let {
          username = null,
          password = null,
@@ -107,12 +69,73 @@
          tls = false
       } = db.getMongo().__serviceProvider.mongoClient.options;
       let readPreference = 'secondaryPreferred';
+      let directURI;
+      if (username == null) {
+         directURI = `mongodb://${hostname}/?directConnection=true&tls=${tls}&compressors=${compressors}&readPreference=${readPreference}`;
+      } else {
+         directURI = `mongodb://${username}:${password}@${hostname}/?directConnection=true&tls=${tls}&authSource=${authSource}&authMechanism=${authMech}&compressors=${compressors}&readPreference=${readPreference}`;
+      }
+      let node = connect(directURI);
+      // console.log(getDBNames());
+      // console.log('listDatabases:', node.getSiblingDB('admin').runCommand({ "listDatabases": 1, "nameOnly": false }, { "readPreference": readPreference }).databases);
+      // console.log('dbstats:', node.getSiblingDB('admin').stats());
+      // console.log('listCollections:', node.getSiblingDB('database').runCommand({ "listCollections": 1, "authorizedCollections": true, "nameOnly": true }, { "readPreference": readPreference }).cursor.firstBatch);
+      // console.log('collStats:', node.getSiblingDB('database').getCollection('collection').aggregate({ "$collStats": { "storageStats": { "scale": 1 } } }).toArray()[0].ns);
+
+      let me = async() => node.hello().me;
+      let stats = async() => node.getSiblingDB('admin').runCommand({ "listDatabases": 1, "nameOnly": false }, { "readPreference": readPreference }).databases;
+      let results = {
+         'process': await me(),
+         'stats': await stats()
+      };
+      return results;
+   }
+
+   function discoverShardedHosts(shards) {
+      /*
+       *
+       */
+      // shards.forEach(({ _id, host }) => console.log(`\nID: ${_id} with: ${host}`));
+      let hosts = shards.map(({ host }) => {
+         // console.log(`\nConnecting to: ${_id}`);
+         let { setName, seedList } = host.match(/(?<setName>\w+)\/(?<seedList>.+)/).groups;
+         return seedList.split(',');
+      });
+      hosts = hosts.flat();
+      hosts = hosts.map(name =>
+         new Object({ 'host': name, 'health': '', 'role': '' })
+      );
+      // let promises = shards.map(fetchShardHosts(setName, seedList));
+
+      // return await Promise.all(promises);
+      // return await Promise.allSettled(promises);
+      return hosts;
+   }
+
+   async function fetchAllStats(hosts) {
+      /*
+       *
+       */
+      let promises = hosts.map(fetchHostStats);
+
+      return Promise.all(promises);
+      // return await Promise.allSettled(promises);
+   }
+
+   async function main() {
+      /*
+       *
+       */
+
+      // load('mdblib.js');
+      // ['ReplSet', 'Sharded', 'LoadBalanced', 'Standalone']
+
       let hosts, shards;
       if (db.hello().msg == 'isdbgrid') { // is mongos process
          shards = discoverShards();
          hosts = discoverShardedHosts(shards);
       } else {
-         hosts = discoverHosts();
+         hosts = discoverRSHosts();
       }
       // console.log(hosts);
       let allHostStats = fetchAllStats(hosts);
