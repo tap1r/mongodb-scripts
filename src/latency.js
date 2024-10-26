99d9cfa2e66f97c8e383bc59f3d4db1af4043b30
Luke Prochazka <luke.prochazka@gmail.com>
2024-10-26 16:56:49 +1100

    Update latency.js

--- src/latency.js
+++ src/latency.js
@@ -1,6 +1,6 @@
 /*
  *  Name: "latency.js"
- *  Version: "0.3.3"
+ *  Version: "0.3.4"
  *  Description: "Driver and network latency telemetry PoC"
  *  Disclaimer: "https://raw.githubusercontent.com/tap1r/mongodb-scripts/master/DISCLAIMER.md"
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
@@ -12,7 +12,7 @@
    /*
     *  main
     */
-   let __script = { "name": "latency.js", "version": "0.3.3" };
+   let __script = { "name": "latency.js", "version": "0.3.4" };
    console.log(`\n\x1b[33m#### Running script ${__script.name} v${__script.version} on shell v${this.version()}\x1b[0m`);
 
    let slowms = 100,
@@ -30,6 +30,7 @@
          { "$project": {
             "_id": 0,
             "slowms": {
+               // deprecated operator in v8, likely to be replaced with a $sleep operator in the future
                "$function": {
                   "body": `function(ms) { sleep(ms) }`,
                   "args": [slowms],
@@ -42,20 +43,35 @@
          "readConcern": { "level": "local" }
       },
       rtt, t0, t1, t2, t3, totalTime, timestamp,
-      report, tableWidth, spacing, hostLength,
-      timeLength, hostname, procType;
-      try {
-         hostname = db.hostInfo()?.system?.hostname ?? 'unknown';
-      } catch(error) {
-         console.log('\x1b[31m[WARN] failed to aquire the hostname:\x1b[0m', error);
-         hostname = 'unknown';
-      }
-      try {
-         procType = db.serverStatus()?.process ?? 'unknown';
-      } catch(error) {
-         console.log('\x1b[31m[WARN] failed to aquire the process type:\x1b[0m', error);
-         procType = 'unknown';
-      }
+      report, tableWidth, spacing = 1, hostLength,
+      timeLength, hostname = 'unknown', procType;
+
+   try {
+      hostname = db.hello().me;
+   } catch(error) {
+      console.log('\x1b[31m[WARN] failed to aquire the hostname:\x1b[0m', error);
+   }
+
+   let [{
+      'tags': {
+         workloadType = '-',
+         availabilityZone = '-',
+         diskState = '-',
+         nodeType = '-',
+         provider = '-',
+         region = '-'
+      } = {}
+   }] = rs.conf().members.filter(
+      ({ host, arbiterOnly, hidden, horizons: { PUBLIC } = {} }) => {
+         return (host == hostname || PUBLIC == hostname) && !arbiterOnly && !hidden;
+   });
+
+   try {
+      procType = db.serverStatus()?.process ?? 'unknown';
+   } catch(error) {
+      console.log('\x1b[31m[WARN] failed to aquire the process type:\x1b[0m', error);
+      procType = 'unknown';
+   }
 
    try {
       t0 = process.hrtime();
@@ -92,35 +108,39 @@
    function fomatted(duration) {
       return Intl.NumberFormat('en', {
          "minimumIntegerDigits": 1,
-         "minimumFractionDigits": 0,
-         "maximumFractionDigits": 2,
+         "minimumFractionDigits": 1,
+         "maximumFractionDigits": 1,
          "style": "unit",
          "unit": "millisecond",
          "unitDisplay": "short"
       }).format(duration);
    }
 
-   spacing = 1;
-   hostLength = 'Target host:'.length + spacing + hostname.length;
+   hostLength = 'Host:'.length + spacing + hostname.length;
    timeLength = 'Timestamp:'.length + spacing + timestamp.length;
    tableWidth = Math.max(hostLength, timeLength);
-   report = `
-   \x1b[1mInternal metrics\x1b[0m
-   \x1b[33m${'━'.repeat(tableWidth)}\x1b[0m
-   \x1b[32m${'Target host:'}\x1b[0m${hostname.padStart(tableWidth - 12)}
-   \x1b[32m${'Process type:'}\x1b[0m${procType.padStart(tableWidth - 13)}
-   \x1b[32m${'Timestamp:'}\x1b[0m${timestamp.padStart(tableWidth - 10)}
-   \x1b[32m${'Delay factor (slowms):'}\x1b[0m${fomatted(slowms).padStart(tableWidth - 22)}
-   \x1b[32m${'Total measurement time:'}\x1b[0m${fomatted(totalTime).padStart(tableWidth - 23)}
-   \x1b[33m${'═'.repeat(tableWidth)}\x1b[0m
-
-   \x1b[1mLatency breakdown\x1b[0m
-   \x1b[33m${'━'.repeat(tableWidth)}\x1b[0m
-   \x1b[32m${'Server execution time:'}\x1b[0m${fomatted(durationMillis - slowms).padStart(tableWidth - 22)}
-   \x1b[32m${'Network latency (RTT):'}\x1b[0m${fomatted(rtt).padStart(tableWidth - 22)}
-   \x1b[32m${'Driver execution time:'}\x1b[0m${fomatted(totalTime - durationMillis - rtt).padStart(tableWidth - 22)}
-   \x1b[33m${'═'.repeat(tableWidth)}\x1b[0m
-   `;
+   report = `\n` +
+      `\x1b[1mInternal metrics\x1b[0m` + `\n` +
+      `\x1b[33m${'━'.repeat(tableWidth)}\x1b[0m` + `\n` +
+      `\x1b[32m${'Host:'}\x1b[0m${hostname.padStart(tableWidth - 'Host:'.length)}` + `\n` +
+      `\x1b[32m${'Process type:'}\x1b[0m${procType.padStart(tableWidth - 'Process type:'.length)}` + `\n` +
+      `\x1b[32m${'Cloud provider:'}\x1b[0m${provider.padStart(tableWidth - 'Cloud provider:'.length)}` + `\n` +
+      `\x1b[32m${'Cloud region:'}\x1b[0m${region.padStart(tableWidth - 'Cloud region:'.length)}` + `\n` +
+      `\x1b[32m${'Availability zone:'}\x1b[0m${availabilityZone.padStart(tableWidth - 'Availability zone:'.length)}` + `\n` +
+      `\x1b[32m${'Disk state:'}\x1b[0m${diskState.padStart(tableWidth - 'Disk state:'.length)}` + `\n` +
+      `\x1b[32m${'Workload type:'}\x1b[0m${workloadType.padStart(tableWidth - 'Workload type:'.length)}` + `\n` +
+      `\x1b[32m${'Node type:'}\x1b[0m${nodeType.padStart(tableWidth - 'Node type:'.length)}` + `\n` +
+      `\x1b[32m${'Timestamp:'}\x1b[0m${timestamp.padStart(tableWidth - 'Timestamp:'.length)}` + `\n` +
+      `\x1b[32m${'Delay factor (slowms):'}\x1b[0m${fomatted(slowms).padStart(tableWidth - 'Delay factor (slowms):'.length)}` + `\n` +
+      `\x1b[32m${'Total measurement time:'}\x1b[0m${fomatted(totalTime).padStart(tableWidth - 'Total measurement time:'.length)}` + `\n` +
+      `\x1b[33m${'═'.repeat(tableWidth)}\x1b[0m` + `\n` +
+      `\n` +
+      `\x1b[1mLatency breakdown\x1b[0m` + `\n` +
+      `\x1b[33m${'━'.repeat(tableWidth)}\x1b[0m` + `\n` +
+      `\x1b[32m${'Server execution time:'}\x1b[0m${fomatted(durationMillis - slowms).padStart(tableWidth - 'Server execution time:'.length)}` + `\n` +
+      `\x1b[32m${'Network latency (RTT):'}\x1b[0m${fomatted(rtt).padStart(tableWidth - 'Network latency (RTT):'.length)}` + `\n` +
+      `\x1b[32m${'Driver execution time:'}\x1b[0m${fomatted(totalTime - durationMillis - rtt).padStart(tableWidth - 'Driver execution time:'.length)}` + `\n` +
+      `\x1b[33m${'═'.repeat(tableWidth)}\x1b[0m` + `\n`;
    console.log(report);
 })();
 
