29e56717c6fa02c9d74dec4a72a535c4cf64b01c
Luke Prochazka <luke.prochazka@gmail.com>
2024-10-28 14:10:38 +1100

    Update rtt.js

--- src/rtt.js
+++ src/rtt.js
@@ -1,7 +1,7 @@
 (() => {
    /*
     *  Name: "rtt.js"
-    *  Version: "0.1.1"
+    *  Version: "0.1.2"
     *  Description: "reports application round trip time latency"
     *  Disclaimer: "https://raw.githubusercontent.com/tap1r/mongodb-scripts/master/DISCLAIMER.md"
     *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
@@ -17,13 +17,13 @@
 
    // Syntax: mongosh [connection options] --quiet [-f|--file] rtt.js
 
-   let __script = { "name": "rtt.js", "version": "0.1.1" };
+   let __script = { "name": "rtt.js", "version": "0.1.2" };
    let banner = `\n\x1b[33m#### Running script ${__script.name} v${__script.version} on shell v${version()}\x1b[0m\n`;
    console.clear();
    console.log(banner);
 
    let servers = () => db.getMongo().__serviceProvider.mongoClient.topology.s.description.servers;
-   let src = db.runCommand({ "whatsmyuri": 1}).you.match(/^(?<src>.+)\:\d+/).groups.src;
+   let src = db.runCommand({ "whatsmyuri": 1}).you.match(/^(?<src>.+)\:(?:\d+)$/).groups.src;
    let latency = (rtt) => // rtt in ms to 1 decimal place
       Intl.NumberFormat('en', {
          "minimumIntegerDigits": 1,
