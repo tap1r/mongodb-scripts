3d0a6b99276d3ce9a63808004529e7bd30af1d80
Luke Prochazka <luke.prochazka@gmail.com>
2024-11-04 11:47:50 +1100

    Update indexCacheUtil.js

--- src/indexCacheUtil.js
+++ src/indexCacheUtil.js
@@ -1,20 +1,31 @@
 /*
  *  Name: "indexCacheUtil.js"
- *  Version: "0.1.2"
- *  Description: index cache util
- *  Disclaimer: https://raw.githubusercontent.com/tap1r/mongodb-scripts/master/DISCLAIMER.md
+ *  Version: "0.1.3"
+ *  Description: "index cache util"
+ *  Disclaimer: "https://raw.githubusercontent.com/tap1r/mongodb-scripts/master/DISCLAIMER.md"
  *  Authors: ["tap1r <luke.prochazka@gmail.com>"]
  *
  *  Notes: mongosh only
+ *
+ *  TODOs:
+ *  - Add threadpool support
+ *  - Add sharding support
+ *  - Convert adminCommand to runCommand for directed command execution
+ *  - Add cluster wide reporting
+ *  - revise the scope for cache util in system namespaces
+ *  - add progress meter will accumulating counters
  */
 
-// Usage: "mongosh [<connection options>] [--quiet] [-f|--file] indexCacheUtil.js"
+// Usage: mongosh [<connection options>] [--quiet] [-f|--file] indexCacheUtil.js
 
 (async() => {
    /*
     *  Index cache util%
     */
    async function getAllNonSystemNamespaces() {
+      /*
+       *  returns a flat array of all non-system namespaces documents
+       */
       return db.adminCommand({
          "listDatabases": 1,
          "filter": {
@@ -22,7 +33,7 @@
          },
          "nameOnly": true,
          "authorizedDatabases": true,
-         "comment": "fetching all namespaces" // fCV(4.4)
+         "comment": "fetching all DB names" // fCV(4.4)
       }).databases.map(async({ 'name': dbName }) => {
          return db.getSiblingDB(dbName).runCommand({
             "listCollections": 1,
@@ -32,7 +43,7 @@
             },
             "nameOnly": true,
             "authorizedCollections": true,
-            "comment": "fetching all namespaces" // fCV(4.4)
+            "comment": "fetching all collection names" // fCV(4.4)
          }).cursor.firstBatch.map(({ 'name': collName }) => {
             return { "dbName": dbName, "collName": collName };
          });
@@ -40,6 +51,24 @@
    }
 
    async function getIndexCacheStats({ dbName, collName }) {
+      /*
+       *  returns the index cache stats for a given namespace
+       */
+
+      // Sample stats
+
+      // wiredTiger: {
+      //    cache: {
+      //      'bytes allocated for updates': 8182,
+      //      'bytes currently in the cache': 69725,
+      //      'bytes dirty in the cache cumulative': 61760,
+      //      'maximum bytes configured': 1073741824,
+      //      'percentage overhead': 8,
+      //      'tracked bytes belonging to internal pages in the cache': 2262,
+      //      'tracked bytes belonging to leaf pages in the cache': 67463,
+      //      'tracked dirty bytes in the cache': 12151
+      // } }
+
       let pipeline = [
          { "$collStats": {
             "storageStats": {
@@ -97,6 +126,9 @@
    }
 
    async function main() {
+      /*
+       *  main
+       */
       let sizeInMemory = 0, totalIndexPages = 0;
       let cacheSize = db.serverStatus().wiredTiger.cache['maximum bytes configured'];
       let cachedPagesBytes = db.serverStatus().wiredTiger.cache['bytes belonging to page images in the cache'];
@@ -116,12 +148,12 @@
          console.log('\n');
          console.log(`\tConfigured WT cache Size:\t${cacheSize} bytes`);
          console.log(`\tTotal bytes in cache:\t\t${cachedPagesBytes} bytes`);
-         console.log(`\tTotal cache util:\t\t${(100 * (cachedPagesBytes / cacheSize)).toFixed(2)}%`);
+         console.log(`\tTotal cache util:\t\t${+(100 * (cachedPagesBytes / cacheSize)).toFixed(2)}%`);
          console.log('\n');
          console.log(`\tTotal indexes size on disk:\t${totalIndexPages} bytes`);
          console.log(`\tIndex bytes in cache:\t\t${sizeInMemory} bytes`);
-         console.log(`\tCache util by indexes:\t\t${(100 * (sizeInMemory / cachedPagesBytes)).toFixed(2)}%`);
-         console.log(`\tIndex working set util:\t\t${(100 * (sizeInMemory / totalIndexPages)).toFixed(2)}%`);
+         console.log(`\tCache util by indexes:\t\t${+(100 * (sizeInMemory / cachedPagesBytes)).toFixed(2)}%`);
+         console.log(`\tIndex working set util:\t\t${+(100 * (sizeInMemory / totalIndexPages)).toFixed(2)}%`);
          console.log('\n');
       });
    }
@@ -129,31 +161,4 @@
    await main();
 })();
 
-// wiredTiger: {
-//    cache: {
-//      'bytes allocated for updates': 8182,
-//      'bytes currently in the cache': 69725,
-//      'bytes dirty in the cache cumulative': 61760,
-//      'maximum bytes configured': 1073741824,
-//      'percentage overhead': 8,
-//      'tracked bytes belonging to internal pages in the cache': 2262,
-//      'tracked bytes belonging to leaf pages in the cache': 67463,
-//      'tracked dirty bytes in the cache': 12151
-// } }
-
-
-// wiredTiger: {
-//    cache: {
-//      'tracked dirty bytes in the cache': 12151
-// } }
-
-// wiredTiger: {
-//    cache: {
-//      'tracked bytes belonging to internal pages in the cache': 2262,
-//      'tracked bytes belonging to leaf pages in the cache': 67463
-// } }
-
-// wiredTiger: {
-//    cache: {
-//      'percentage overhead': 8
-// } }
+// EOF
